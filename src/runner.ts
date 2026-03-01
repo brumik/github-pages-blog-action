3017fa20e732e334c514c9a4d8e115f2187fe314
Levente Berky <levente.berky@gmail.com>
2026-03-01 10:01:20 +0100

    Update runner.ts

--- src/runner.ts
+++ src/runner.ts
@@ -6,11 +6,11 @@ import { deploy } from './deploy';
 export async function run(configuration: ConfigurationType) {
   info('Checking configuration and starting deployment');
 
-  try {
-    //await configureRepo(configuration);
-  } catch (e: any) {
-    throw new Error(`There was an error initializing the repository: ${e.message}`);
-  }
+  //try {
+  //  await configureRepo(configuration);
+  //} catch (e: any) {
+  //  throw new Error(`There was an error initializing the repository: ${e.message}`);
+  //}
 
   await prepareTheme(configuration);
   //await deploy(configuration);
